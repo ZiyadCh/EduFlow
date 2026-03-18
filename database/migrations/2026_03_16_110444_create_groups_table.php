9114ed4d1ecded9347f53dd8cd156c2b637b831e
ZiyadCh <chetoziyad@gmail.com>
2026-03-18 14:30:04 +0000

    added 0 as default value for group table, also group limit condition works

--- database/migrations/2026_03_16_110444_create_groups_table.php
+++ database/migrations/2026_03_16_110444_create_groups_table.php
@@ -13,7 +13,7 @@ return new class extends Migration {
         Schema::create('groups', function (Blueprint $table) {
             $table->id();
             $table->foreignId('course_id')->constrained()->cascadeOnDelete();
-            $table->integer('membres');
+            $table->integer('membres')->default(0);
             $table->timestamps();
         });
     }
