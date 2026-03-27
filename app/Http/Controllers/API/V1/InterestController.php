8aca1f3ee195c02f36630c2b182eca628774f426
ZiyadCh <chetoziyad@gmail.com>
2026-03-27 12:32:49 +0100

    created interest controller

---
+++ app/Http/Controllers/API/V1/InterestController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\API\V1;
+
+use App\Models\Interest;
+use Illuminate\Http\Request;
+
+class InterestController
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Interest $interest)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Interest $interest)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Interest $interest)
+    {
+        //
+    }
+}
