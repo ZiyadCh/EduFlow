271d2cf69c93b206f3ed8836c4839e48e50e9c8b
ZiyadCh <chetoziyad@gmail.com>
2026-03-30 10:51:02 +0100

    list of course show in the page

--- public/js/course.js
+++ public/js/course.js
@@ -3,8 +3,36 @@ async function fetchCourses() {
     const response = await fetch("/api/v1/courses");
     if (!response.ok) {
         console.log("ereeur");
+        return;
     }
     const data = await response.json();
+    container.innerHTML = data
+        .map(
+            (course) => `
+    <div class='relative bg-white border border-slate-200 rounded-2xl p-6 shadow-sm'>
+
+                <button class='absolute top-4 right-4 text-slate-300 hover:text-red-500 text-xl'>
+                    ❤
+                </button>
+
+                <div class='flex flex-col h-full'>
+                    <p class='text-xs font-bold uppercase text-blue-600 mb-1'>
+                        Instructor ID: ${course.teacher_id}
+                    </p>
+
+                    <h3 class='text-lg font-extrabold text-slate-900 mb-6'>
+                        ${course.topic}
+                    </h3>
+
+                    <div class='mt-auto pt-4 border-t border-slate-50'>
+                        <span class='text-xl font-black text-slate-900'>
+                            ${course.price}
+                        </span>
+                    </div>
+                </div>
+            </div>`,
+        )
+        .join("");
     console.log(data);
 }
 
