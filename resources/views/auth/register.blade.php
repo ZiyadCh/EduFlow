dffd60a81736fce64d80ff74ea9b2edc183b6495
ZiyadCh <chetoziyad@gmail.com>
2026-03-31 17:18:55 +0100

    renamed auth.js to register.js

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>EduFlow | Inscription</title>
-    <script src="{{ asset('js/auth.js') }}" defer></script>
+    <script src="{{ asset('js/register.js') }}" defer></script>
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700;800&display=swap" rel="stylesheet">
     <style>body { font-family: 'Inter', sans-serif; }</style>
