072094779b1e9f4e29e970999c29d5d6b1c3e812
ZiyadCh <chetoziyad@gmail.com>
2026-04-01 12:47:15 +0100

    moved courses and search js to a courses directory

--- resources/views/students/courses.blade.php
+++ resources/views/students/courses.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.student')
-<script src="{{ asset('/js/course.js') }}" defer></script>
-<script src="{{ asset('/js/search.js') }}" defer></script>
+<script src="{{ asset('/js/courses/show-courses.js') }}" defer></script>
+<script src="{{ asset('/js/courses/search.js') }}" defer></script>
 
 
 @section('content')
